e53833dea8b9734f5c386c7730d95941f86e8c69
sdadoun <simon.dad@gmail.com>
2020-05-14 13:15:27 +0200

    add packages

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var hash = require('pbkdf2-password');
+var path = require('path');
+var session = require('express-session');
+
+
 /* GET login. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Route Login' });
